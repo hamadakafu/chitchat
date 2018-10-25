b0312ae86792c6e7968e58d5f3a476ada81f8416
hamadakafu <kafu.h1998@gmail.com>
2018-10-25 12:46:21 +0900

    fixed main.go ssl

--- main.go
+++ main.go
@@ -6,37 +6,18 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
 	"net/http"
+    "log"
 
 	_ "github.com/lib/pq"
 	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,                  // Let's Encryptの利用規約への同意
-		HostPolicy: autocert.HostWhitelist("localhost"), // ドメイン名
-		Cache:      autocert.DirCache("certs"),          // 証明書などを保存するフォルダ
-	}
-	challengeServer := &http.Server{
-		Handler: certManager.HTTPHandler(nil),
-		Addr:    ":8080",
-	}
-	go challengeServer.ListenAndServe()
-	server := &http.Server{
-		Addr: ":443",
-		TLSConfig: &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		},
-	}
-
-	http.HandleFunc("/entrance", entrance)
-	http.HandleFunc("/home", home)
-	if err := server.ListenAndServeTLS("", ""); err != nil {
-		fmt.Println(err)
-	}
+    mux := http.NewServeMux()
+	mux.HandleFunc("/entrance", entrance)
+	mux.HandleFunc("/home", home)
+    log.Fatal(http.Serve(autocert.NewListener("www.kafu-tech.xyz"), mux))
 }
 
 // func main() {
